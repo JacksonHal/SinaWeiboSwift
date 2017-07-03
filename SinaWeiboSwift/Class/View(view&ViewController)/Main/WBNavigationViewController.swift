a95bf6e9928702e934d20cab73c263a565f557ec
好杰 <2120235218@qq.com>
2017-07-03 15:08:17 +0800

    项目结构的搭建

---
+++ SinaWeiboSwift/Class/View(view&ViewController)/Main/WBNavigationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBNavigationViewController.swift
+//  SinaWeiboSwift
+//
+//  Created by meixiang02 on 2017/7/3.
+//  Copyright © 2017年 好杰. All rights reserved.
+//
+
+import UIKit
+
+class WBNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
